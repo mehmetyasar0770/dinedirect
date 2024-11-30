3b1441e9a79472f619705bc2a7d3b4cf948e6072
Mehmet Yasar <mehmet.yasar0770@gmail.com>
2024-11-30 23:09:44 +0300

    typo: text düzeltme

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -13,7 +13,7 @@ function NotFound() {
       />
       <h1 className="text-5xl font-bold text-red-500 mb-4">404</h1>
       <p className="text-xl text-gray-700 mb-4">
-        Oops! Aradığınız sayfa bulunamadı.
+        Oops! Yanlış yola girdiniz.
       </p>
       <p className="text-lg text-gray-600 mb-6">
         Ama endişelenmeyin, mutfağımıza geri dönebilirsiniz!
