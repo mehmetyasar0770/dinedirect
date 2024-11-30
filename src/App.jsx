d9dcbe475a4ae29b3d70a09ae49ef8c45948a0c3
Mehmet Yasar <mehmet.yasar0770@gmail.com>
2024-11-30 23:41:32 +0300

    feat: firebase config

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
           element:<AdminDashboard/>,
         },
         {
-          path:"/*",
+          path:"*",
           element:<NotFound/>,
         },
 
---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDvuSXV4YfsNPD7nGv54O145owY5vkm08c",
+    authDomain: "dinedirect-22d97.firebaseapp.com",
+    projectId: "dinedirect-22d97",
+    storageBucket: "dinedirect-22d97.firebasestorage.app",
+    messagingSenderId: "556479502125",
+    appId: "1:556479502125:web:e21f0a25bab17025c7e5a4",
+    measurementId: "G-9EVCSX3R53"
+};
+const app = initializeApp (firebaseConfig);
+export const auth = getAuth (app);
